39b12257cabf66da955f3ce12212b7270aaf4d7e
edoardesd <edoardesd@gmail.com>
2022-07-28 17:18:19 +0200

    add multi location in control fragment

--- app/src/main/java/com/example/smartparking/ui/parking/control/ControlFragment.kt
+++ app/src/main/java/com/example/smartparking/ui/parking/control/ControlFragment.kt
@@ -1,11 +1,15 @@
 package com.example.smartparking.ui.parking.control
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -14,6 +18,7 @@ import com.example.smartparking.databinding.ControlFragmentBinding
 import com.example.smartparking.internal.LoadingDialog
 import com.example.smartparking.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.control_fragment.*
+import kotlinx.android.synthetic.main.navigation_choice_fragment.*
 
 
 class ControlFragment : ScopedFragment() {
@@ -21,6 +26,9 @@ class ControlFragment : ScopedFragment() {
     private lateinit var loadingDialog : LoadingDialog
     private lateinit var binding: ControlFragmentBinding
     private val controlViewModel: ControlViewModel by viewModels()
+
+    private var selectedIndexParking : Int = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +47,23 @@ class ControlFragment : ScopedFragment() {
         initFreeSlots()
     }
 
+    override fun onResume() {
+        super.onResume()
+        initDropdownMenu()
+    }
+
+    private fun initDropdownMenu() {
+        val parkingLocations = resources.getStringArray(R.array.parking_location)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_parking, parkingLocations)
+
+        binding.actvParkingLocation.setAdapter(arrayAdapter)
+
+        actv_parking_location.setOnItemClickListener { _, _, position, _ ->
+            selectedIndexParking = position
+            Log.d(ContentValues.TAG, "Selected parking at position: $position")
+        }
+    }
+
     private fun initFreeSlots() {
         controlViewModel.slotsPrediction.observe(viewLifecycleOwner, Observer { freeSlots ->
             if(freeSlots == null) return@Observer
@@ -47,10 +72,27 @@ class ControlFragment : ScopedFragment() {
     }
 
     private fun initStream() {
-        controlViewModel.bitmap.observe(viewLifecycleOwner, Observer { bitmap ->
+        controlViewModel.bitmapDASTU.observe(viewLifecycleOwner, Observer { bitmap ->
             if ( bitmap == null) return@Observer
-            loadingDialog.dismiss()
-            iv_mqtt.setImageBitmap(bitmap)
+            if (selectedIndexParking == 1){
+                loadingDialog.dismiss()
+                iv_mqtt.setImageBitmap(bitmap)
+                Log.d(TAG, "GET dastu")
+                Log.d(TAG, "$bitmap")
+                Log.d(TAG, "selected parking $selectedIndexParking")
+            }
+        })
+
+        controlViewModel.bitmapDEIB.observe(viewLifecycleOwner, Observer { bitmap ->
+            if ( bitmap == null) return@Observer
+            if (selectedIndexParking == 0) {
+                loadingDialog.dismiss()
+                iv_mqtt.setImageBitmap(bitmap)
+                Log.d(TAG, "GET deib")
+                Log.d(TAG, "$bitmap")
+                Log.d(TAG, "selected parking $selectedIndexParking")
+
+            }
         })
     }
 
--- app/src/main/java/com/example/smartparking/ui/parking/control/ControlViewModel.kt
+++ app/src/main/java/com/example/smartparking/ui/parking/control/ControlViewModel.kt
@@ -3,31 +3,30 @@ package com.example.smartparking.ui.parking.control
 import android.app.Application
 import android.content.ContentValues.TAG
 import android.graphics.Bitmap
-import android.graphics.Bitmap.createBitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
 import android.util.Log
-import androidx.core.graphics.createBitmap
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import com.example.smartparking.data.db.RoomDetails
 import com.example.smartparking.data.network.MQTTConnectionParams
 import com.example.smartparking.data.network.MQTTManager
 import com.example.smartparking.data.network.MQTTMessage.MQTTMessage
 import com.google.gson.Gson
-import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
 import java.io.UnsupportedEncodingException
 import java.util.*
 
 const val serverURI = "tcp://broker.hivemq.com:1883"
-const val TOPIC_IMAGE = "smart/parking/polimi/image"
-const val TOPIC_PREDICTION = "smart/parking/polimi/prediction"
+const val TOPIC_IMAGE = "smart/parking/polimi/deib/image"
+const val TOPIC_PREDICTION = "smart/parking/polimi/deib/prediction"
+const val TOPIC_IMAGE_DASTU = "smart/parking/polimi/dastu/image"
+const val TOPIC_PREDICTION_DASTU = "smart/parking/polimi/dastu/prediction"
 
 
 class ControlViewModel(app: Application) : AndroidViewModel(app){
 
-    private var _bitmap: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
+    private var _bitmapDEIB: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
+    private var _bitmapDASTU: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
     private var _slotsPrediction: MutableLiveData<Int> = MutableLiveData<Int>()
     private val context = getApplication<Application>().applicationContext
     private val uniqueID : String = "parking" + UUID.randomUUID().toString()
@@ -48,10 +47,19 @@ class ControlViewModel(app: Application) : AndroidViewModel(app){
             override fun messageArrived(topic:String, mqttMessage: MqttMessage) {
                 Log.d(TAG, "Received message from topic: $topic")
                 if (topic == TOPIC_IMAGE) {
-                    decodeMessage(mqttMessage)
+                    decodeMessage(mqttMessage, topic)
                 }
                 if (topic == TOPIC_PREDICTION){
                     _slotsPrediction.value = mqttMessage.toString().toInt()
+                }
+
+                if (topic == TOPIC_IMAGE_DASTU) {
+                    decodeMessage(mqttMessage, topic)
+                    Log.d(TAG, "image dastu")
+                }
+                if (topic == TOPIC_PREDICTION_DASTU){
+//                    _slotsPrediction.value = mqttMessage.toString().toInt()
+                    Log.d(TAG, "prediction dastu")
                 }
 
             }
@@ -62,7 +70,7 @@ class ControlViewModel(app: Application) : AndroidViewModel(app){
 
     fun subscribe(topic: String, qos: Int = 1){
         try {
-            mqttManager.getMQTTClient().subscribe(arrayOf(topic, TOPIC_PREDICTION), intArrayOf(qos, 2), null, object:IMqttActionListener {
+            mqttManager.getMQTTClient().subscribe(arrayOf(topic, TOPIC_PREDICTION, TOPIC_IMAGE_DASTU, TOPIC_PREDICTION_DASTU), intArrayOf(qos, 2, 2, 2), null, object:IMqttActionListener {
                 override fun onSuccess(asyncActionToken:IMqttToken) {
                     Log.d(TAG, "Subscribed to $topic")                }
                 override fun onFailure(asyncActionToken:IMqttToken, exception:Throwable) {
@@ -75,7 +83,7 @@ class ControlViewModel(app: Application) : AndroidViewModel(app){
         }
     }
 
-        fun decodeMessage(message: MqttMessage?) {
+        fun decodeMessage(message: MqttMessage?, topic: String) {
             val messageJson = Gson().fromJson(message.toString(), MQTTMessage::class.java)
             val decodedImageBytes: ByteArray
             try {
@@ -90,14 +98,28 @@ class ControlViewModel(app: Application) : AndroidViewModel(app){
 
             Log.d(TAG, "Image decoded.")
             val tmpBitmap : Bitmap = BitmapFactory.decodeByteArray(decodedImageBytes, 0, decodedImageBytes.size)
-            _bitmap.value = tmpBitmap
+
+            if (topic == TOPIC_IMAGE) {
+                _bitmapDEIB.value = tmpBitmap
+                Log.d(TAG, "bitmap deib")
+            }
+
+            if (topic == TOPIC_IMAGE_DASTU) {
+                _bitmapDASTU.value = tmpBitmap
+                Log.d(TAG, "bitmap dastu")
+
+            }
 
             Log.d(TAG, "Image passed to view.")
     }
 
-    internal var bitmap : MutableLiveData<Bitmap>
-        get() {return _bitmap}
-        set(value) {_bitmap = value}
+    internal var bitmapDEIB : MutableLiveData<Bitmap>
+        get() {return _bitmapDEIB}
+        set(value) {_bitmapDEIB = value}
+
+    internal var bitmapDASTU : MutableLiveData<Bitmap>
+        get() {return _bitmapDASTU}
+        set(value) {_bitmapDASTU = value}
 
     internal var slotsPrediction : MutableLiveData<Int>
         get() {return _slotsPrediction}
